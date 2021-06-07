f81317e7fadee1e2a5209f3887521f27a42778e4
Philipp Scheer <scheer28philipp@gmail.com>
2021-06-07 20:24:38 +0200

    improvements

--- html/backend/filters.py
+++ html/backend/filters.py
@@ -1,17 +1,43 @@
-from __main__ import *
+"""
+Copyright (c) 2021 Philipp Scheer
+"""
+
+
+from __main__ import app, datetime, format_datetime
+import time
+import datetime as dt
 import markdown
+
 
 @app.template_filter("datetime")
 def filter_datetime(value, format="full"):
     dt = datetime.utcfromtimestamp(int(value))
     str = f"Unknown format {format}"
-    if format == "time":
+    if format in ["time", "short"]:
         str = format_datetime(dt, "HH:mm:ss")
-    elif format == "date":
+    elif format in ["date"]:
         str = format_datetime(dt, "EE, d. MMMM yyyy")
-    elif format == "full":
+    elif format in ["full", "long"]:
         str = format_datetime(dt, "EEEE, d. MMMM yyyy HH:mm:ss")
     return str
+
+@app.template_filter("timediff")
+def filter_timediff(value):
+    diff  = time.time() - value
+    delta = dt.timedelta(seconds=diff)
+    res   = []
+
+    d,h,m = delta.days, delta.seconds//3600, (delta.seconds//60)%60
+    if d > 0:
+        res.append(f"{delta.days} days")
+    if h > 0:
+        res.append(f"{delta.seconds//3600} hour" + "s" if h != 1 else "")
+    if m > 0:
+        res.append(f"{m} minutes")
+
+    if len(res) == 0: # everything under 1 minute is considered just now
+        return "Just now"
+    return ", ".join(res) + " ago"
 
 @app.template_filter("markdown")
 def filter_markdown(value):
